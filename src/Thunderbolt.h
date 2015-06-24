411af4b5fa9e2607e882c0b4cfc3a65e97033fb5
Dan Quigley <dan@tipsolutions.com>
2015-06-23 20:53:30 -0700

    Update Thunderbolt.h

--- src/Thunderbolt.h
+++ src/Thunderbolt.h
@@ -52,8 +52,6 @@ class Thunderbolt
 public:
 
 	Thunderbolt(Stream* _serial);
-	//Thunderbolt(int serial_num);
-
 	Stream*	getSerial();
 	bool getSoftwareVersionInfo();
 	bool setFixMode(ReportType pos_fixmode, ReportType vel_fixmode, AltMode alt = ALT_NOCHANGE, PPSMode pps = PPS_NOCHANGE, GPSTimeMode time = TME_NOCHANGE);
